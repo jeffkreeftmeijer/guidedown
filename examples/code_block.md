149a753e05d4106f3d2a06ae16b8a153961b2be8
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2015-10-26 20:54:15 +0100

    Guidedown doesn't include paragraphs in code blocks

--- examples/code_block.md
+++ examples/code_block.md
@@ -1,3 +1,5 @@
+This is a paragraph.
+
     def foo
       puts 'bar'
     end
--- guidedown_cli_test.rb
+++ guidedown_cli_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 
 describe "Guidedown's command line interface" do
   it "converts indented code blocks to fenced code blocks" do
-    assert_equal "```\ndef foo\n  puts 'bar'\nend\n```\n", 
+    assert_equal "This is a paragraph.\n\n```\ndef foo\n  puts 'bar'\nend\n```\n",
       `bin/guidedown examples/code_block.md`
   end
 end

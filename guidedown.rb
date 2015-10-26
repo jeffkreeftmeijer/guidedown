ec734c099858df139d7f6a6f6283406c357a2b04
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2015-10-26 20:35:34 +0100

    Guidedown#to_s handles multiline code blocks

--- guidedown.rb
+++ guidedown.rb
@@ -4,6 +4,6 @@ class Guidedown
   end
 
   def to_s
-    "```\n#{@input.sub('    ', '')}\n```"
+    "```\n#{@input.gsub('    ', '')}\n```"
   end
 end
--- guidedown_test.rb
+++ guidedown_test.rb
@@ -5,4 +5,8 @@ describe Guidedown do
   it "converts indented code blocks to fenced code blocks" do
     assert_equal "```\nfoo\n```", Guidedown.new('    foo').to_s
   end
+
+  it "converts indented multiline code blocks to fenced code blocks" do
+    assert_equal "```\nfoo\nbar\n```", Guidedown.new("    foo\n    bar").to_s
+  end
 end
